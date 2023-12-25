6848649b45bab18aed7daf62bf98bbf382d7a8fa
kiraGit67 <kirsten@sais.de>
2023-12-25 18:53:50 +0100

    replaces const msg by const header, removes input

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { ref } from 'vue'
-const msg = ref('Hello World')
+
+const header = ref('Shopping List App')
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <input v-model="msg" type="text" />
+  <h1>{{ header }}</h1>
 </template>
