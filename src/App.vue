81792610d2760473ba24a314a1a1c75fc9acbeb8
kiraGit67 <kirsten@sais.de>
2023-12-26 21:43:34 +0100

    adds newItem, newItemHighPriority, adds header and form.add-item-form elements

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,43 @@ const items = ref([
   { id: 2, count: 25, label: 'DVD Labels' },
   { id: 3, count: 40, label: 'USB-Sticks' }
 ])
+
+const newItem = ref('')
+//const newItemPriority = ref('low')
+const newItemHighPriority = ref(false)
 </script>
 
 <template>
-  <h1>{{ header }}</h1>
+  <header>
+    <h1>{{ header }}</h1>
+  </header>
+  <form class="add-item-form">
+    <input
+      v-model.trim="newItem"
+      type="text"
+      name="new-item"
+      id="new-item"
+      placeholder="Add new item"
+    />
+    <!--
+    <label for="priority">
+      Priority:
+      <select v-model="newItemPriority" name="priority" id="priority">
+        <option value="low">Low</option>
+        <option value="high">High</option>
+      </select>
+    </label>
+    -->
+    <label for="high-priority">
+      <input
+        v-model="newItemHighPriority"
+        type="checkbox"
+        name="high-priority"
+        id="high-priority"
+      />
+      High Priority
+    </label>
+  </form>
   <ul>
     <li v-for="({ id, count, label }, index) in items" :key="id">
       [{{ index }}] {{ id }}) {{ count }} {{ label }}
